c02cd282fff47686eaf19bd42e189f07c480fcb4
ariesjmustika <ariesjmustika@gmail.com>
2022-02-16 17:26:52 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\DataController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// Route::post('pusat/tambahatk', [PusatKelolaATKController::class, 'tambahATK'])->name('tambahatk');
+Route::post('register',  [UserController::class, 'register']);
+Route::post('login',  [UserController::class, 'authenticate']);
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', [UserController::class, 'getAuthenticatedUser']);
+    Route::get('closed', [DataController::class, 'closed']);
+});
